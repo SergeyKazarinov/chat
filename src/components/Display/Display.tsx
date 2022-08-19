53cbf1bf57a6a0ca35beeff0df08a892e2b164a7
Sergey Kazarinov <kazarinov092@gmail.com>
2022-08-19 21:29:31 +0500

    feat(components) add form component

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import display from './Display.module.css';
+import Form from "../Form/Form";
 
 const Display:React.FC = () => {
   return(
     <div className={display.container}>
       <div className={display.display}>
-        
+        <Form />
       </div>
     </div>
   )
