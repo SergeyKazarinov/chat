bd6ac3b05542ec38c96de3d87efbb97b6c56304f
Sergey Kazarinov <kazarinov092@gmail.com>
2022-08-20 23:45:50 +0500

    feat(messages): add Context for messages and add method add message on display

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { CurrentMessageContext } from "../../Context/CurrentMessageContext";
 import OtherMessage from "../OtherMessage/OtherMessage";
 import UserMessage from "../UserMessage/UserMessage";
 import messages from './Messages.module.css'
 
-type arrayMessages = {
+type itemMessages = {
   id: number,
   isOwn: boolean,
   name: string,
@@ -11,17 +12,12 @@ type arrayMessages = {
   time: string,
 }
 
-const Messages:React.FC<{msgs: Array<arrayMessages>}> = ({msgs}) => {
-  const scrollRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-
-    scrollRef.current?.scrollIntoView(false)
-  }, [msgs])
+const Messages:React.FC = () => {
+  const messageContext = useContext(CurrentMessageContext);
 
   return(
-    <div className={messages.container} ref={scrollRef}>
-      {msgs.map((item: { id: number, isOwn: boolean, name: string, text: string, time: string }, index: number) => (
+    <div className={messages.container}>
+      {messageContext.map((item: itemMessages, index: number) => (
         item.isOwn == true
         ? <UserMessage  key={index} msgs={item}/>
         : <OtherMessage key={index} msgs={item}/>
